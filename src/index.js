e82228c0bc13164228bcc14335d2894a76685d83
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-09 19:24:49 +0530

    Comparisons with the Logical Or Operator

--- src/index.js
+++ src/index.js
@@ -477,6 +477,17 @@ function testLessOrEqual(val) {
   
   testLogicalAnd(10);
 
+  //Comparisons with the Logical Or Operator
+
+  function testLogicalOr(val) {
+  
+    if (val< 10 || val>20) {
+      return "Outside";
+    }
+    return "Inside";
+  }
+  
+  testLogicalOr(15);
 
   
   
