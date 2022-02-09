de86fdb69838cbecad025b84d762984b891398c0
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-09 18:01:36 +0530

    Practice comparing different values

--- src/index.js
+++ src/index.js
@@ -354,7 +354,6 @@ function testEqual(val) {
 
 //   Comparison with the Strict Equality Operator
 
-// Setup
 function testStrict(val) {
     if (val===7) { 
       return "Equal";
@@ -363,6 +362,18 @@ function testStrict(val) {
   }
   
   testStrict(10);
+
+
+//  Practice comparing different values
+
+function compareEquality(a, b) {
+    if (a === b) { 
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  compareEquality(10, "10");
 
 
 
