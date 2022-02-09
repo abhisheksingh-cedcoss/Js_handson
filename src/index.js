d067c6cfae3dbcf39de1a263d076d5a8571aa641
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-09 19:12:26 +0530

    Comparison with the Less Than Operator

--- src/index.js
+++ src/index.js
@@ -428,7 +428,20 @@ function testStrictNotEqual(val) {
   
   testGreaterOrEqual(10);
 
-
+//  Comparison with the Less Than Operator
+function testLessThan(val) {
+    if (val<25) {  
+      return "Under 25";
+    }
+  
+    if (val<55) {  
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+  }
+  
+  testLessThan(10);
 
 
 
