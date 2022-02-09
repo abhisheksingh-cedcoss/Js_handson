957369e0eae3d3eca0f53db45f3423e581e54f33
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-09 16:38:48 +0530

    Access Multi-Dimensional Arrays With Indexes

--- src/index.js
+++ src/index.js
@@ -191,6 +191,16 @@ const myData = myArray[0];
 const myArray = [18, 64, 99];
 myArray[0]= 45;
 
+//Access Multi-Dimensional Arrays With Indexes
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+  const myData = myArray[2][1];
+
 
 
 
