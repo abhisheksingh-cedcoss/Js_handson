d57955bf3b5d1feb3ef24fce78ba4f59fce2d567
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-09 19:42:33 +0530

    chaining the if else statement

--- src/index.js
+++ src/index.js
@@ -538,6 +538,35 @@ function testLessOrEqual(val) {
   }
   
   orderMyLogic(7);
+
+
+  // Chaining If Else Statements
+  function testSize(num) {
+  if(num<5){
+    return "Tiny";
+  }
+  else if(num<10){
+    return "Small";
+  }
+  
+  else if(num<15){
+    return"Medium";
+  }
+  
+  else if(num<20){
+    return "Large";
+  }
+  
+  else if(num>=20){
+    return "Huge";
+  
+  }
+  else{
+  
+    return "Change Me";}
+  }
+  
+  testSize(7);
   
 
 
