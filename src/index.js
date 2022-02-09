02e4d3a045d77695e1897339a5d567a8bd091b10
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-09 16:49:39 +0530

    Manipulate Arrays With pop()

--- src/index.js
+++ src/index.js
@@ -206,6 +206,11 @@ const myArray = [
 const myArray = [["John", 23], ["cat", 2]];
 myArray.push(["dog",3]);
 
+//Manipulate Arrays With pop()
+const myArray = [["John", 23], ["cat", 2]];
+const removedFromMyArray = myArray.pop();
+
+
 
 
 
