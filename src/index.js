d994c8994a01ac91d63315a49de056b75177471a
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-09 17:13:15 +0530

    Passing Values to Functions with Arguments

--- src/index.js
+++ src/index.js
@@ -230,6 +230,20 @@ function reusableFunction(){
   reusableFunction();
 
 
+//Passing Values to Functions with Arguments
+ 
+function functionWithArgs(a,b){
+    console.log(a+b);
+  
+  }
+  
+  functionWithArgs(1,2);
+  functionWithArgs(7,9);
+  
+  
+
+
+
 
 
 
