47268a96dbd33e6231cff63a46fe65947f2c2d6c
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-09 19:04:07 +0530

    Comparison with the Strict Inequality Operator

--- src/index.js
+++ src/index.js
@@ -385,6 +385,18 @@ function testNotEqual(val) {
   
   testNotEqual(10);
 
+  //Comparison with the Strict Inequality Operator
+function testStrictNotEqual(val) {
+    if (val!==17) { 
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testStrictNotEqual(10);
+
+  
+
 
 
 
