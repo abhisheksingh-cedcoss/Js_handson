fdb8cf937710524d9f0f426bcebdeb5130f3d877
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-09 17:53:47 +0530

    Use Conditional Logic with If Statements

--- src/index.js
+++ src/index.js
@@ -328,7 +328,18 @@ function welcomeToBooleans() {
   
   }
 
+//Use Conditional Logic with If Statements
 
+function trueOrFalse(wasThatTrue) {
+  if(wasThatTrue){
+    return "Yes, that was true";
+  }
+  return "No, that was false";
+  
+    
+  }
+  
+  
 
   
   
