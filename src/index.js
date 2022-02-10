8874e83b68931ee8356ede34377dd60a56410935
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-10 12:26:43 +0530

    Accessing Nested Objects

--- src/index.js
+++ src/index.js
@@ -917,6 +917,26 @@ const myMusic = [
 
 ];
 
+//  Accessing Nested Objects
+
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
+
+
+
+
+
 
 
 
