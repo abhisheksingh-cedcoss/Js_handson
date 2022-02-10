20b4c214a55a404806725e60673d0a678db2d60e
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-10 10:56:59 +0530

    Add New Properties to a JavaScript Object

--- src/index.js
+++ src/index.js
@@ -834,6 +834,16 @@ const myDog = {
 
 myDog.name= "Happy Coder";
 
+// Add New Properties to a JavaScript Object
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark ="woof";
+
   
 
 
