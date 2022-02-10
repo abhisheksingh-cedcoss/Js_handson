88bd887635f150eb353a5ca2aa7d68e4163e83e1
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-10 10:58:45 +0530

    Delete Properties from a JavaScript Object

--- src/index.js
+++ src/index.js
@@ -844,6 +844,18 @@ const myDog = {
 
 myDog.bark ="woof";
 
+
+// Delete Properties from a JavaScript Object
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+ delete myDog.tails;
+
   
 
 
