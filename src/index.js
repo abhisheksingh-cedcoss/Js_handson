fe75983e582fabde6319da5d1d5a5b4cdd808b66
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-10 14:46:17 +0530

    Use Recursion to Create a Countdown

--- src/index.js
+++ src/index.js
@@ -1176,3 +1176,14 @@ function convertToInteger(str) {
   }
   
   checkSign(10);
+
+  //Use Recursion to Create a Countdown
+function countdown(n){
+  if (n < 1) {
+    return [];
+  } else {
+    const arr = countdown(n - 1);
+    arr.unshift(n);
+    return arr;
+  }
+}
