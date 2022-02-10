a75b825df9c664f4f95cc2288c8cc167543001bb
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-10 11:28:39 +0530

    Manipulating Complex Objects

--- src/index.js
+++ src/index.js
@@ -892,6 +892,31 @@ function checkObj(obj, checkProp) {
 }
 
 
+// Manipulating Complex Objects
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+
+{
+  "artist": "Adam",
+  "title": "Drumer",
+  "release_year": 2004,
+  "formate": ["Youtube"]
+}
+
+
+
+];
+
 
 
 
