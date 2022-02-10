66258893f515f9c0e2906f14d0d77ba927be65db
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-10 14:44:43 +0530

    Use Multiple Conditional (Ternary) Operators

--- src/index.js
+++ src/index.js
@@ -1169,3 +1169,10 @@ function convertToInteger(str) {
   }
   
   checkEqual(1, 2);
+
+  // Use Multiple Conditional (Ternary) Operators
+  function checkSign(num) {
+    return num > 0 ? "positive" : num < 0 ? "negative" : "zero"
+  }
+  
+  checkSign(10);
