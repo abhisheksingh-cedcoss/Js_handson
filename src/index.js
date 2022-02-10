2caa8a75e5555646d639e06ece4ceea0036b9054
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-10 13:35:11 +0530

    Iterate with JavaScript Do...While Loops

--- src/index.js
+++ src/index.js
@@ -1059,6 +1059,17 @@ for (var i =0; i<arr.length; i++){
 
 multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
 
+// Iterate with JavaScript Do...While Loops
+
+
+const myArray = [];
+let i = 10;
+
+do  {
+  myArray.push(i);
+  i++;
+}
+while (i < 5)
 
 
 
