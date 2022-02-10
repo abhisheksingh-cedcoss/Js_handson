792b29d3babac2ec0d1516a5be263df3fe9d0984
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-10 11:11:12 +0530

    Using Objects for Lookups

--- src/index.js
+++ src/index.js
@@ -856,7 +856,26 @@ const myDog = {
 
  delete myDog.tails;
 
+ // Using Objects for Lookups
+
+function phoneticLookup(val) {
+  let result = "";
+
+  const lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie":"Chicago",
+    "delta":"Denver",
+    "echo": "Easy",
+    "foxtrot":"Frank"
+  };
+   result = lookup [val];
   
+
+  return result;
+}
+
+phoneticLookup("charlie"); 
 
 
 
