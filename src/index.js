4ed792390f5e982c0eb3a224ae2b6dc1cc464556
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-10 10:42:54 +0530

    Accessing Object Properties with Bracket Notation

--- src/index.js
+++ src/index.js
@@ -798,7 +798,18 @@ const testObj = {
 };
 
 const hatValue = testObj.hat;     
-const shirtValue = testObj.shirt;    
+const shirtValue = testObj.shirt;   
+
+//  Accessing Object Properties with Bracket Notation
+
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+const entreeValue = testObj["an entree"];   
+const drinkValue = testObj["the drink"];    
 
   
 
